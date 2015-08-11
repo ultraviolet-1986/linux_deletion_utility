ae2daa6534898f7503aa2549ce572325bb91af2f
William Willis <w_willis86@hotmail.co.uk>
2015-08-11 17:33:34 +0100

    Initial Commit

---
+++ LinuxDeletionUtility/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace LinuxDeletionUtility
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			frmMain win = new frmMain ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
---
+++ LinuxDeletionUtility/frmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+
+public partial class frmMain: Gtk.Window
+{
+	public frmMain () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+	protected void menuAbout_Click (object sender, EventArgs e)
+	{
+		// About the Author.
+		AboutDialog about = new AboutDialog();
+		about.ProgramName = "Linux Deletion Utility";
+		about.Version = "0.1";
+		about.Copyright = "© William Willis Whinn";
+		about.Comments = "This is a simple application to delete unnecessary files.";
+		about.Website = @"https://github.com/ultraviolet-1986";
+		about.Run();
+		about.Destroy();
+	}
+}
