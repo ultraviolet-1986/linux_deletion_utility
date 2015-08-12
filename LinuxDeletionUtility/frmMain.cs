6b10e0d4403607335512f172deb4421545d254c7
William Willis <w_willis86@hotmail.co.uk>
2015-08-12 14:23:27 +0100

    Tidy and organise code

--- LinuxDeletionUtility/frmMain.cs
+++ LinuxDeletionUtility/frmMain.cs
@@ -3,33 +3,16 @@ using Gtk;
 
 public partial class frmMain: Gtk.Window
 {
+	// #############################################################################
+	// MAIN FORM FUNCTIONS
+	// #############################################################################
+
+	#region mainFormFunctions
+
 	public frmMain () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 		resetApplication ();
-	}
-
-	public void resetApplication()
-	{
-		// Clear application console.
-		txtConsole.Buffer.Clear ();
-
-		// Display the first page of the notebook.
-		noteMain.CurrentPage = 0;
-
-		// Clear all Home Folder checkboxes.
-		chkMostRecentlyUsed.Active = false;
-		chkCommandLineHistory.Active = false;
-		chkWastebasket.Active = false;
-		chkImageThumbnails.Active = false;
-	}
-
-	public void checkboxEnabled(bool state)
-	{
-		chkMostRecentlyUsed.Sensitive = state;
-		chkCommandLineHistory.Sensitive = state;
-		chkWastebasket.Sensitive = state;
-		chkImageThumbnails.Sensitive = state;
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -38,6 +21,60 @@ public partial class frmMain: Gtk.Window
 		a.RetVal = true;
 	}
 
+	#endregion
+
+	// #############################################################################
+	// ADDITIONAL FORM FUNCTIONS
+	// #############################################################################
+
+	#region additionalFormFunctions
+
+	public void checkboxEnabled (bool state)
+	{
+		// Home Folder Checkboxes
+		chkMostRecentlyUsed.Sensitive = state;
+		chkCommandLineHistory.Sensitive = state;
+		chkWastebasket.Sensitive = state;
+		chkImageThumbnails.Sensitive = state;
+	}
+
+	public void resetApplication ()
+	{
+		// Clear Application Console.
+		txtConsole.Buffer.Clear ();
+
+		// Display the first Page of the Notebook.
+		noteMain.CurrentPage = 0;
+
+		// Clear all Home Folder Checkboxes.
+		chkMostRecentlyUsed.Active = false;
+		chkCommandLineHistory.Active = false;
+		chkWastebasket.Active = false;
+		chkImageThumbnails.Active = false;
+	}
+
+	public void performTask ()
+	{
+		checkboxEnabled (false);
+
+		// Placeholder text to demonstrate how to write to the Console.
+		txtConsole.Buffer.Clear ();
+		txtConsole.Buffer.InsertAtCursor ("Now beginning cleaning Procedure...\n");
+		txtConsole.Buffer.InsertAtCursor ("Performing Cleanup...\n");
+		txtConsole.Buffer.InsertAtCursor ("Cleanup Complete.");
+
+		checkboxEnabled (true);
+	}
+
+	#endregion
+
+	// #############################################################################
+	// CLICK EVENTS
+	// #############################################################################
+
+	#region clickEvents
+
+	// MENU ITEM CLICK EVENTS
 	protected void menuExit_Click (object sender, EventArgs e)
 	{
 		Application.Quit ();
@@ -45,37 +82,27 @@ public partial class frmMain: Gtk.Window
 
 	protected void menuAbout_Click (object sender, EventArgs e)
 	{
-		AboutDialog about = new AboutDialog();
+		AboutDialog about = new AboutDialog ();
 		about.ProgramName = "Linux Deletion Utility";
 		about.Version = "0.1";
 		about.Copyright = "(c) William Willis Whinn";
 		about.Comments = "This is a simple application to delete unnecessary files inside a user's Home folder.";
 		about.Website = @"https://github.com/ultraviolet-1986/LinuxDeletionUtility";
-		about.Run();
-		about.Destroy();
+		about.Run ();
+		about.Destroy ();
 	}
 
+	// BUTTON CLICK EVENTS
 	protected void btnClean_Click (object sender, EventArgs e)
 	{
 		if (chkMostRecentlyUsed.Active == false &&
 		    chkCommandLineHistory.Active == false &&
 		    chkWastebasket.Active == false &&
-		    chkImageThumbnails.Active == false)
-		{
+		    chkImageThumbnails.Active == false) {
 			txtConsole.Buffer.Clear ();
 			txtConsole.Buffer.InsertAtCursor ("You have not selected any items to delete.");
-		}
-		else
-		{
-			checkboxEnabled (false);
-
-			// Placeholder text to demonstrate how to write to the console.
-			txtConsole.Buffer.Clear ();
-			txtConsole.Buffer.InsertAtCursor ("Now beginning cleaning Procedure...\n");
-			txtConsole.Buffer.InsertAtCursor ("Performing Cleanup...\n");
-			txtConsole.Buffer.InsertAtCursor ("Cleanup Complete.");
-
-			checkboxEnabled (true);
+		} else {
+			performTask ();
 		}
 	}
 
@@ -83,4 +110,6 @@ public partial class frmMain: Gtk.Window
 	{
 		resetApplication ();
 	}
+
+	#endregion
 }
