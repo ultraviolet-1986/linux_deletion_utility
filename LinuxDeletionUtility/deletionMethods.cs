c092f9b8e8985fb0b2cadb1c33e7e95192641aa0
William Willis <w_willis86@hotmail.co.uk>
2015-08-12 16:07:39 +0100

    Add Thumbnail Cache deletion

--- LinuxDeletionUtility/deletionMethods.cs
+++ LinuxDeletionUtility/deletionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace LinuxDeletionUtility
@@ -6,29 +7,58 @@ namespace LinuxDeletionUtility
 	public class deletionMethods
 	{
 		// #############################################################################
+		// MAIN DELETION FUNCTIONS
+		// #############################################################################
+
+		#region mainDeletionFunctions
+
+		public static void changeToHome ()
+		{
+			string homePath = Environment.GetEnvironmentVariable ("HOME");
+			Environment.CurrentDirectory = homePath;
+		}
+
+		public static void deleteFiles (string path)
+		{
+			DirectoryInfo deletionTarget = new DirectoryInfo (path);
+
+			foreach (FileInfo file in deletionTarget.GetFiles()) {
+				file.Delete (); 
+			}
+
+			foreach (DirectoryInfo directory in deletionTarget.GetDirectories()) {
+				directory.Delete (true); 
+			}
+		}
+
+		#endregion
+
+		// #############################################################################
 		// HOME FOLDER DELETION FUNCTIONS
 		// #############################################################################
 
 		#region homeFolderDeletionFunctions
 
-		protected void cleanMostRecentlyUsed ()
+		public static void cleanMostRecentlyUsed ()
 		{
 			// Insert Cleaner code here.
 		}
 
-		protected void cleanCommandLineHistory ()
+		public static void cleanCommandLineHistory ()
 		{
 			// Insert Cleaner code here.
 		}
 
-		protected void cleanWastebasket ()
+		public static void cleanWastebasket ()
 		{
 			// Insert Cleaner code here.
 		}
 
-		protected void cleanImageThumbnails ()
+		public static void cleanImageThumbnails ()
 		{
-			// Insert Cleaner code here.
+			changeToHome ();
+			string imageThumbnailPath = @".cache/thumbnails";
+			deleteFiles (imageThumbnailPath);
 		}
 
 		#endregion
--- LinuxDeletionUtility/frmMain.cs
+++ LinuxDeletionUtility/frmMain.cs
@@ -1,113 +1,112 @@
 ﻿using System;
 using Gtk;
+using LinuxDeletionUtility;
 
-namespace LinuxDeletionUtility
+public partial class frmMain: Gtk.Window
 {
-	public partial class frmMain: Gtk.Window
+	// #############################################################################
+	// MAIN FORM FUNCTIONS
+	// #############################################################################
+
+	#region mainFormFunctions
+
+	public frmMain () : base (Gtk.WindowType.Toplevel)
 	{
-		// #############################################################################
-		// MAIN FORM FUNCTIONS
-		// #############################################################################
-
-		#region mainFormFunctions
-
-		public frmMain () : base (Gtk.WindowType.Toplevel)
-		{
-			Build ();
-			resetApplication ();
-		}
-
-		protected void OnDeleteEvent (object sender, DeleteEventArgs a)
-		{
-			Application.Quit ();
-			a.RetVal = true;
-		}
-
-		#endregion
-
-		// #############################################################################
-		// ADDITIONAL FORM FUNCTIONS
-		// #############################################################################
-
-		#region additionalFormFunctions
-
-		public void checkboxEnabled (bool state)
-		{
-			chkMostRecentlyUsed.Sensitive = state;
-			chkCommandLineHistory.Sensitive = state;
-			chkWastebasket.Sensitive = state;
-			chkImageThumbnails.Sensitive = state;
-		}
-
-		public void resetApplication ()
-		{
-			noteMain.CurrentPage = 0;
-
-			txtConsole.Buffer.Clear ();
-
-			chkMostRecentlyUsed.Active = false;
-			chkCommandLineHistory.Active = false;
-			chkWastebasket.Active = false;
-			chkImageThumbnails.Active = false;
-		}
-
-		public void performTask ()
-		{
-			checkboxEnabled (false);
-
-			txtConsole.Buffer.Clear ();
-			txtConsole.Buffer.InsertAtCursor ("Now beginning cleaning Procedure...\n");
-			txtConsole.Buffer.InsertAtCursor ("Performing Cleanup...\n");
-			txtConsole.Buffer.InsertAtCursor ("Cleanup Complete.");
-
-			checkboxEnabled (true);
-		}
-
-		#endregion
-
-		// #############################################################################
-		// CLICK EVENTS
-		// #############################################################################
-
-		#region clickEvents
-
-		// MENU ITEM CLICK EVENTS
-		protected void menuExit_Click (object sender, EventArgs e)
-		{
-			Application.Quit ();
-		}
-
-		protected void menuAbout_Click (object sender, EventArgs e)
-		{
-			AboutDialog about = new AboutDialog ();
-			about.ProgramName = "Linux Deletion Utility";
-			about.Version = "0.1";
-			about.Copyright = "(c) William Willis Whinn";
-			about.Comments = "This is a simple application to delete unnecessary files inside a user's Home folder.";
-			about.Website = @"https://github.com/ultraviolet-1986/LinuxDeletionUtility";
-			about.Run ();
-			about.Destroy ();
-		}
-
-		// BUTTON CLICK EVENTS
-		protected void btnClean_Click (object sender, EventArgs e)
-		{
-			if (chkMostRecentlyUsed.Active == false &&
-			    chkCommandLineHistory.Active == false &&
-			    chkWastebasket.Active == false &&
-			    chkImageThumbnails.Active == false) {
-				txtConsole.Buffer.Clear ();
-				txtConsole.Buffer.InsertAtCursor ("You have not selected any items to delete.");
-			} else {
-				performTask ();
-			}
-		}
-
-		protected void btnReset_Click (object sender, EventArgs e)
-		{
-			resetApplication ();
-		}
-
-		#endregion
+		Build ();
+		resetApplication ();
 	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	#endregion
+
+	// #############################################################################
+	// ADDITIONAL FORM FUNCTIONS
+	// #############################################################################
+
+	#region additionalFormFunctions
+
+	public void checkboxEnabled (bool state)
+	{
+		chkMostRecentlyUsed.Sensitive = state;
+		chkCommandLineHistory.Sensitive = state;
+		chkWastebasket.Sensitive = state;
+		chkImageThumbnails.Sensitive = state;
+	}
+
+	public void resetApplication ()
+	{
+		noteMain.CurrentPage = 0;
+
+		txtConsole.Buffer.Clear ();
+
+		chkMostRecentlyUsed.Active = false;
+		chkCommandLineHistory.Active = false;
+		chkWastebasket.Active = false;
+		chkImageThumbnails.Active = false;
+	}
+
+	public void performTask ()
+	{
+		checkboxEnabled (false);
+
+		txtConsole.Buffer.Clear ();
+
+		if (chkImageThumbnails.Active == true) {
+			deletionMethods.cleanImageThumbnails ();
+		}
+
+		checkboxEnabled (true);
+	}
+
+	#endregion
+
+	// #############################################################################
+	// CLICK EVENTS
+	// #############################################################################
+
+	#region clickEvents
+
+	// MENU ITEM CLICK EVENTS
+	protected void menuExit_Click (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+
+	protected void menuAbout_Click (object sender, EventArgs e)
+	{
+		AboutDialog about = new AboutDialog ();
+		about.ProgramName = "Linux Deletion Utility";
+		about.Version = "0.1";
+		about.Copyright = "(c) William Willis Whinn";
+		about.Comments = "This is a simple application to delete unnecessary files inside a user's Home folder.";
+		about.Website = @"https://github.com/ultraviolet-1986/LinuxDeletionUtility";
+		about.Run ();
+		about.Destroy ();
+	}
+
+	// BUTTON CLICK EVENTS
+	protected void btnClean_Click (object sender, EventArgs e)
+	{
+		if (chkMostRecentlyUsed.Active == false &&
+		    chkCommandLineHistory.Active == false &&
+		    chkWastebasket.Active == false &&
+		    chkImageThumbnails.Active == false) {
+			txtConsole.Buffer.Clear ();
+			txtConsole.Buffer.InsertAtCursor ("You have not selected any items to delete.");
+		} else {
+			performTask ();
+		}
+	}
+
+	protected void btnReset_Click (object sender, EventArgs e)
+	{
+		resetApplication ();
+	}
+
+	#endregion
 }
