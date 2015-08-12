cfd67541f283027ad8d9f6ec480caa793c475d4c
William Willis <w_willis86@hotmail.co.uk>
2015-08-12 15:16:45 +0100

    Add deletionMethods.cs boilerplate code

--- LinuxDeletionUtility/deletionMethods.cs
+++ LinuxDeletionUtility/deletionMethods.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
 
 namespace LinuxDeletionUtility
 {
 	public class deletionMethods
 	{
-		public deletionMethods ()
+		// #############################################################################
+		// HOME FOLDER DELETION FUNCTIONS
+		// #############################################################################
+
+		#region homeFolderDeletionFunctions
+
+		protected void cleanMostRecentlyUsed ()
 		{
+			// Insert Cleaner code here.
 		}
+
+		protected void cleanCommandLineHistory ()
+		{
+			// Insert Cleaner code here.
+		}
+
+		protected void cleanWastebasket ()
+		{
+			// Insert Cleaner code here.
+		}
+
+		protected void cleanImageThumbnails ()
+		{
+			// Insert Cleaner code here.
+		}
+
+		#endregion
 	}
 }
-
