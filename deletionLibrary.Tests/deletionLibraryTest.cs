e5c75f06996a5effed928b80869e3afb472f6b4c
William Willis <w_willis86@hotmail.co.uk>
2015-08-13 12:00:51 +0100

    Add goHomeTest case

---
+++ deletionLibrary.Tests/deletionLibraryTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using deletionLibrary;
+
+namespace deletionLibrary.Tests
+{
+	[TestFixture ()]
+	public class deletionLibraryTest
+	{
+		[Test ()]
+		public void goHomeTest ()
+		{
+			// NAVIGATE TO THE CURRENT USER'S HOME FOLDER
+			string homePath = Environment.GetEnvironmentVariable ("HOME");
+			Environment.CurrentDirectory = homePath;
+
+			// ENSURE THE CORRECT PATH TO WORK FROM HAS BEEN LOADED
+			Assert.AreEqual (Environment.CurrentDirectory, homePath);
+		}
+	}
+}
