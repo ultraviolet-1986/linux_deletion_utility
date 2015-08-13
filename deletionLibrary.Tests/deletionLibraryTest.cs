213537a34d99ee1794608517a007fa9d198b124e
William Willis <w_willis86@hotmail.co.uk>
2015-08-13 13:19:13 +0100

    Separate and test individual functions

--- deletionLibrary.Tests/deletionLibraryTest.cs
+++ deletionLibrary.Tests/deletionLibraryTest.cs
@@ -13,8 +13,8 @@ namespace deletionLibrary.Tests
 
 		#region mainTestVariables
 
-		const string TESTFILE = ".test_file";
-		const string TESTSTRING = "Hello World!";
+		const string TEST_FILE = ".test_file";
+		const string TEST_STRING = "Hello World!";
 
 		#endregion
 
@@ -36,28 +36,74 @@ namespace deletionLibrary.Tests
 		}
 
 		[Test ()]
-		public void manageFileTest ()
+		public void createFileTest ()
 		{
 			// NAVIGATE TO THE CURRENT USER'S HOME FOLDER
-			goHomeTest ();
-
-			// CREATE THE TESTFILE IF NOT EXISTS AND INPUT A STRING
-			File.WriteAllText (TESTFILE, TESTSTRING);
-
-			// MAKE SURE FILE HAS HAD TESTSTRING WRITTEN
-			Assert.True (new FileInfo (TESTFILE).Length > 0);
+			string homePath = Environment.GetEnvironmentVariable ("HOME");
+			Environment.CurrentDirectory = homePath;
 
 			// CREATE THE TESTFILE IF NOT EXISTS AND CLEAR ALL TEXT
-			File.WriteAllText (TESTFILE, string.Empty);
+			File.WriteAllText (TEST_FILE, string.Empty);
 
 			// MAKE SURE FILE HAS BEEN CLEARED OF ALL TEXT
-			Assert.True (new FileInfo (TESTFILE).Length == 0);
+			Assert.True (new FileInfo (TEST_FILE).Length == 0);
 
 			// DELETE THE FILE
-			File.Delete (TESTFILE);
+			File.Delete (TEST_FILE);
+		}
+
+		[Test ()]
+		public void writeToFileTest ()
+		{
+			// NAVIGATE TO THE CURRENT USER'S HOME FOLDER
+			string homePath = Environment.GetEnvironmentVariable ("HOME");
+			Environment.CurrentDirectory = homePath;
+
+			// CREATE THE TESTFILE IF NOT EXISTS AND INPUT A STRING
+			File.WriteAllText (TEST_FILE, TEST_STRING);
+
+			// MAKE SURE FILE HAS HAD TESTSTRING WRITTEN TO IT
+			Assert.True (new FileInfo (TEST_FILE).Length > 0);
+
+			// DELETE THE FILE
+			File.Delete (TEST_FILE);
+		}
+
+		[Test ()]
+		public void deleteFileContentTest ()
+		{
+			// NAVIGATE TO THE CURRENT USER'S HOME FOLDER
+			string homePath = Environment.GetEnvironmentVariable ("HOME");
+			Environment.CurrentDirectory = homePath;
+
+			// CREATE THE TESTFILE IF NOT EXISTS AND INPUT A STRING
+			File.WriteAllText (TEST_FILE, TEST_STRING);
+
+			// DELETE ALL TEXT FROM THE 'TESTFILE'
+			File.WriteAllText (TEST_FILE, string.Empty);
+
+			// MAKE SURE FILE HAS HAD TESTSTRING ERASED FROM IT
+			Assert.True (new FileInfo (TEST_FILE).Length == 0);
+
+			// DELETE THE FILE
+			File.Delete (TEST_FILE);
+		}
+
+		[Test ()]
+		public void deleteFileTest ()
+		{
+			// NAVIGATE TO THE CURRENT USER'S HOME FOLDER
+			string homePath = Environment.GetEnvironmentVariable ("HOME");
+			Environment.CurrentDirectory = homePath;
+
+			// CREATE AND THEN DELETE THE 'TESTFILE'
+			File.WriteAllText (TEST_FILE, string.Empty);
+
+			// DELETE THE FILE
+			File.Delete (TEST_FILE);
 
 			// ENSURE FILE HAS BEEN DELETED
-			Assert.False (File.Exists (TESTFILE));
+			Assert.False (File.Exists (TEST_FILE));
 		}
 
 		#endregion
