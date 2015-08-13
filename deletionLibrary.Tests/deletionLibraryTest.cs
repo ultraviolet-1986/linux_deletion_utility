23b64ab8028084161ed3d1c7887cbed67a9a8bca
William Willis <w_willis86@hotmail.co.uk>
2015-08-13 13:00:06 +0100

    Update deletionLibraryTest Unit Tests

--- deletionLibrary.Tests/deletionLibraryTest.cs
+++ deletionLibrary.Tests/deletionLibraryTest.cs
@@ -1,12 +1,29 @@
 ﻿using NUnit.Framework;
 using System;
-using deletionLibrary;
+using System.IO;
 
 namespace deletionLibrary.Tests
 {
 	[TestFixture ()]
 	public class deletionLibraryTest
 	{
+		// #############################################################################
+		// MAIN TEST VARIABLES
+		// #############################################################################
+
+		#region mainTestVariables
+
+		const string TESTFILE = ".test_file";
+		const string TESTSTRING = "Hello World!";
+
+		#endregion
+
+		// #############################################################################
+		// MAIN UNIT TESTS
+		// #############################################################################
+
+		#region mainUnitTests
+
 		[Test ()]
 		public void goHomeTest ()
 		{
@@ -17,5 +34,32 @@ namespace deletionLibrary.Tests
 			// ENSURE THE CORRECT PATH TO WORK FROM HAS BEEN LOADED
 			Assert.AreEqual (Environment.CurrentDirectory, homePath);
 		}
+
+		[Test ()]
+		public void manageFileTest ()
+		{
+			// NAVIGATE TO THE CURRENT USER'S HOME FOLDER
+			goHomeTest ();
+
+			// CREATE THE TESTFILE IF NOT EXISTS AND INPUT A STRING
+			File.WriteAllText (TESTFILE, TESTSTRING);
+
+			// MAKE SURE FILE HAS HAD TESTSTRING WRITTEN
+			Assert.True (new FileInfo (TESTFILE).Length > 0);
+
+			// CREATE THE TESTFILE IF NOT EXISTS AND CLEAR ALL TEXT
+			File.WriteAllText (TESTFILE, string.Empty);
+
+			// MAKE SURE FILE HAS BEEN CLEARED OF ALL TEXT
+			Assert.True (new FileInfo (TESTFILE).Length == 0);
+
+			// DELETE THE FILE
+			File.Delete (TESTFILE);
+
+			// ENSURE FILE HAS BEEN DELETED
+			Assert.False (File.Exists (TESTFILE));
+		}
+
+		#endregion
 	}
 }
